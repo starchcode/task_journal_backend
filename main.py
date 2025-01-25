0930dcde01442a618230ee6524e7bf0cd35b2112
starchcode <mrdave67@gmail.com>
2025-01-25 19:59:17 +0000

    allows CORS to my app

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 import models
 from database import engine 
 from api import tasks
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 models.Base.metadata.create_all(bind=engine)
 
 @app.get('/')
