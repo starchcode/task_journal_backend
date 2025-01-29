927e9d2bb4adfb5ba2203cf7cc252cf789cc0cb7
starchcode <mrdave67@gmail.com>
2025-01-29 12:14:18 +0000

    can embed but api returns error

--- api/tasks.py
+++ api/tasks.py
@@ -2,28 +2,51 @@ from fastapi import APIRouter, HTTPException
 import models
 import schemas
 import dependencies
+from openai import OpenAI
+client = OpenAI()
 
 app = APIRouter()
 
 db = dependencies.db_dependency
 
+# Helper function to generate embeddings using OpenAI
+async def generate_embedding(text: str) -> list:
+    response = client.embeddings.create(
+        input=text,
+        model="text-embedding-ada-002"
+    )
+
+    return response.data[0].embedding
+
 @app.get('/tasks')
 async def get_tasks(db:db):
 
-    result = db.query(models.Tasks).order_by(models.Tasks.deadline.asc(), models.Tasks.id.asc()).all()
+    result = db.query(models.Tasks.id,
+                      models.Tasks.title,
+                      models.Tasks.description,
+                      models.Tasks.deadline,
+                      models.Tasks.is_completed
+                      ).order_by(models.Tasks.deadline.asc(), models.Tasks.id.asc()).all()
 
     return result
 
 
 @app.post("/tasks")
 async def create_questions(task: schemas.TaskBase, db:db):
+    title_embedding = await generate_embedding(task.title)
+    description_embedding = []
+
+    if task.description:
+        description_embedding = await generate_embedding(task.description)
+
     new_task = models.Tasks(title=task.title,
-                               description= task.description,
-                               deadline=task.deadline,
-                               )
+                            description= task.description,
+                            deadline=task.deadline,
+                            title_embedding=title_embedding,
+                            description_embedding=description_embedding
+                            )
 
     try:
-        # Add to session and commit
         db.add(new_task)
         db.commit()
         db.refresh(new_task)
--- models.py
+++ models.py
@@ -4,11 +4,12 @@ from database import Base
 from pgvector.sqlalchemy import Vector
 
 class Tasks(Base):
-    __tablename__ = 'tasks'
+    __tablename__ = 'task_journal'
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(Text) 
     deadline = Column(Date, default=func.current_date())
     is_completed = Column(Boolean, default=False)
-    embedding = Column(Vector(1536))
+    title_embedding = Column(Vector(1536)) 
+    description_embedding = Column(Vector(1536)) 
