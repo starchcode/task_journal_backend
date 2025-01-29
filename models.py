254e5cb5c2e52c423c0c31b98b14bb4e3c97b2d7
starchcode <mrdave67@gmail.com>
2025-01-29 11:06:43 +0000

    updated model to take in vector column for tasks

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Boolean, Column, Integer, String, Text, Date
 from sqlalchemy.sql import func
 from database import Base
+from pgvector.sqlalchemy import Vector
 
 class Tasks(Base):
     __tablename__ = 'tasks'
@@ -10,3 +11,4 @@ class Tasks(Base):
     description = Column(Text) 
     deadline = Column(Date, default=func.current_date())
     is_completed = Column(Boolean, default=False)
+    embedding = Column(Vector(1536))
